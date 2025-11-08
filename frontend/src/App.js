5d9a5e8b204a5b6bea901bf376a470ab064816b8
vilvt <saigehatfield@gmail.com>
2025-11-08 14:02:22 -0500

    troubleshooting react-router-dom error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+//import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./login";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
