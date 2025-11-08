faea32d6ee25a8dd9abbf9438a1ed98349059d0d
Simran Malik <smalik12@charlotte.edu>
2025-11-08 11:28:24 -0500

    database

--- db/database.py
+++ db/database.py
@@ -1,10 +1,13 @@
-# database.py
+# db/database.py
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://<username>:<password>@cluster.mongodb.net/")
-DB_NAME = "tundra"
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://<user>:<pass>@cluster.mongodb.net/")
+DB_NAME = os.getenv("DB_NAME", "tundra")
 
 client = AsyncIOMotorClient(MONGO_URI)
 db = client[DB_NAME]
+
+# collections you define here
 agents_collection = db["agents"]
+jobs_collection = db["jobs"]
